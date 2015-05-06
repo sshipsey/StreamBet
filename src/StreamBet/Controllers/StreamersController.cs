9d493e9189b92d0608a5ebf4ee68d4df7939136e
Steve Shipsey <sashipsey@gmail.com>
2015-05-06 00:11:41 -0400

    A little roughed up, but added Controller functionality for accessing Streamers from an HTTP request

---
+++ src/StreamBet/Controllers/StreamersController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using StreamBet.Models;
+using Microsoft.AspNet.Mvc;
+
+namespace StreamBet.Controllers
+{
+    [Route("api/[controller]")]
+    public class StreamersController : Controller
+    {
+        private IRegistrationRepo _registrationRepo;
+
+        public StreamersController(IRegistrationRepo registrationRepo)
+        {
+            _registrationRepo = registrationRepo;
+        }
+
+        [HttpGet]
+        public IEnumerable<Streamer> GetAllStreamers()
+        {
+            return _registrationRepo.GetStreamers();
+        }
+
+        [HttpGet("{id:int}", Name = "GetStreamerById")]
+        public IActionResult GetStreamerById(int id)
+        {
+            var streamer = _registrationRepo.GetStreamer(id);
+            if (streamer == null)
+            {
+                return HttpNotFound();
+            }
+            return new ObjectResult(streamer);
+        }
+
+    }
+}
--- src/StreamBet/Models/StreamerDbContext.cs
+++ src/StreamBet/Models/StreamerDbContext.cs
@@ -10,7 +10,8 @@ namespace StreamBet.Models
 
         protected override void OnConfiguring(DbContextOptionsBuilder options)
         {
-            options.UseSqlServer(Startup.Configuration.Get("Data:DefaultConnection: ConnectionString"));
+            //options.UseSqlServer(Startup.Configuration.Get("Data:DefaultConnection: ConnectionString"));
+            options.UseSqlServer("Server=tcp:streambet.database.windows.net,1433;Database=StreamBet;User ID=sshipsey@streambet;Password=xxxx;Trusted_Connection=False;Encrypt=True;Connection Timeout=30;");
         }
     }
 }
