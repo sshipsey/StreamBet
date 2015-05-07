77ec451e8640d8960d3eab4532d946f5404fd1b4
perrym5 <muffinman616@gmail.com>
2015-05-06 22:41:51 -0400

    Moved streamers controller

---
+++ src/StreamBet/Api/StreamersController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using StreamBet.Models;
+using Microsoft.AspNet.Mvc;
+
+namespace StreamBet.Controllers
+{
+    [Route("api/[controller]")]
+    public class StreamersController : Controller
+    {
+        private IRegistrationRepo _registrationRepo;
+
+        public StreamersController(IRegistrationRepo registrationRepo)
+        {
+            _registrationRepo = registrationRepo;
+        }
+
+        [HttpGet]
+        public IAsyncEnumerable<Streamer> GetAllStreamers()
+        {
+            return _registrationRepo.GetStreamers();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetStreamerById(int id)
+        {
+            var streamer = await _registrationRepo.GetStreamerAsync(id);
+            if (streamer == null)
+            {
+                return HttpNotFound();
+            }
+
+            return new ObjectResult(streamer);
+        }
+
+    }
+}
