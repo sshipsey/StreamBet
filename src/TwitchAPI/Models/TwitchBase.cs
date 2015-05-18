1659878c521169bc6132c6dc71c6ea53712414ca
Steve Shipsey <Steven.Shipsey@novelis.com>
2015-05-18 17:06:23 -0400

    Added project for twitch API. Started laying out basic models.

---
+++ src/TwitchAPI/Models/TwitchBase.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace TwitchAPI.Models
+{
+    public abstract class TwitchBase
+    {
+        [JsonProperty("error")]
+        public string Error { get; set; }
+        
+        [JsonProperty("message")]
+        public string Message { get; set; }            
+    }
+}
---
+++ src/TwitchAPI/Models/User.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace TwitchAPI.Models
+{
+    public class User : TwitchBase
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("display_name")]
+        public string DisplayName { get; set; }
+
+        [JsonProperty("_id")]
+        public string Id { get; set; }
+
+        [JsonProperty("created_at")]
+        public string CreatedAt { get; set; }
+
+        [JsonProperty("updated_at")]
+        public string UpdatedAt { get; set; }
+
+        [JsonProperty("_links")]
+        public Dictionary<string, object> Links { get; set; }
+
+        [JsonProperty("logo")]
+        public string Logo { get; set; }
+        
+        [JsonProperty("staff")]
+        public string Staff { get; set; }
+    }
+}
